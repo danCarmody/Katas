0853d4c096c4ba310c678dd6da4dab4d4b0b267d
danCarmody <danCarmody93@gmail.com>
2020-05-15 14:17:35 -0700

    tester file for coin count, recursive and dynamic

---
+++ python/Counting Change/count_test.py
@@ -0,0 +1,16 @@
+def count(s, m, n):
+     # Base Case #1 (if n reaches zero, whole amount, possible solution):
+     if (n == 0):
+         return 1
+
+    # Base Case #2 (if n is less than zero, no solution exists):
+    if (n<0):
+        return 0
+
+    # Base Case #3 (If there are no coins and some value to compare to, no solution):
+    if (m<=0 and n>=1):
+        return 0
+
+    return count(s,m-1, n) + count(s,m, n-s[m-1])
+
+    print(count([1,2,], 2, 4))
